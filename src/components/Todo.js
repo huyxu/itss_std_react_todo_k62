d61d42110b15649a800fea15cbb516a070d51e22
huyxu <ec2-user@ip-172-31-75-69.ec2.internal>
2021-04-08 09:13:32 +0000

    Add new Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -38,11 +38,18 @@ function Todo() {
     putItems(newItems);
   }
 
+  const handleAdd = (text) => {
+    putItems([...items, { key: getKey(), text: text, done: false }]);
+  }
+
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
+      
+      <Input onAdd={handleAdd}/>
+      
       {items.map(item => (
         <TodoItem 
           key={item.key}
